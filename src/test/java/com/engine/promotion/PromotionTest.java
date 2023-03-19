8b08cd0aeb596a26dece9647237bbeba26a96a6f
ansuman0077 <ansumansamantroy@gmail.com>
2023-03-19 14:13:22 +0000

    Added test case for promotion not applicable scenario -fixed price N items

--- src/test/java/com/engine/promotion/PromotionTest.java
+++ src/test/java/com/engine/promotion/PromotionTest.java
@@ -8,12 +8,13 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PromotionTest {
 
     @Test
-    public void testBuyNItemsForFixedPricePromotionIsApplicable() {
+    public void promotionIsApplicableScenarioForFixedPriceNItems() {
         Promotion promotion = new FixedPriceNItemsOneSkuPromotion("A", 3, 130.0);
         List<Sku> skus = new ArrayList<>();
         skus.add(new Sku("A", 50));
@@ -21,5 +22,15 @@ public class PromotionTest {
         skus.add(new Sku("A", 50));
         skus.add(new Sku("B", 30));
         assertTrue(promotion.isApplicable(skus));
+    }
+
+    @Test
+    public void promotionIsNotApplicableScenarioForFixedPriceNItems() {
+        Promotion promotion = new FixedPriceNItemsOneSkuPromotion("A", 3, 130.0);
+        List<Sku> skus = new ArrayList<>();
+        skus.add(new Sku("A", 50));
+        skus.add(new Sku("A", 50));
+        skus.add(new Sku("B", 30));
+        assertFalse(promotion.isApplicable(skus));
     }
 }
