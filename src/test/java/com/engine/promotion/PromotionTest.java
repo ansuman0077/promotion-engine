bd1ceb1b4d77b28197a3ac91bee2b2b8427be7d3
ansuman0077 <ansumansamantroy@gmail.com>
2023-03-19 15:21:46 +0000

    Added Junit test cases for Fixed Price N items scenarios

--- src/test/java/com/engine/promotion/PromotionTest.java
+++ src/test/java/com/engine/promotion/PromotionTest.java
@@ -8,18 +8,26 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PromotionTest {
 
     @Test
-    public void promotionIsApplicableScenarioForFixedPriceNItems() {
+    public void promotionIsApplicableScenarioOneForFixedPriceNItems() {
         Promotion promotion = new FixedPriceNItemsOneSkuPromotion("A", 3, 130.0);
         List<Sku> skus = new ArrayList<>();
         skus.add(new Sku("A", 50));
         skus.add(new Sku("A", 50));
         skus.add(new Sku("A", 50));
+        skus.add(new Sku("B", 30));
+        assertTrue(promotion.isApplicable(skus));
+    }
+
+    @Test
+    public void promotionIsApplicableScenarioTwoForFixedPriceNItems() {
+        Promotion promotion = new FixedPriceNItemsOneSkuPromotion("B", 2, 45.0);
+        List<Sku> skus = new ArrayList<>();
+        skus.add(new Sku("B", 30));
         skus.add(new Sku("B", 30));
         assertTrue(promotion.isApplicable(skus));
     }
@@ -32,5 +40,29 @@ public class PromotionTest {
         skus.add(new Sku("A", 50));
         skus.add(new Sku("B", 30));
         assertFalse(promotion.isApplicable(skus));
+    }
+
+    @Test
+    public void applyPromotionScenarioOneForFixedPriceNItems() {
+        Promotion promotion = new FixedPriceNItemsOneSkuPromotion("A", 3, 130.0);
+        List<Sku> skus = new ArrayList<>();
+        skus.add(new Sku("A", 50));
+        skus.add(new Sku("A", 50));
+        skus.add(new Sku("A", 50));
+        skus.add(new Sku("B", 30));
+        double discount = promotion.applyPromotion(skus);
+        assertNotNull(discount);
+        assertEquals(20.0,discount);
+    }
+
+    @Test
+    public void applyPromotionScenarioTwoForFixedPriceNItems() {
+        Promotion promotion = new FixedPriceNItemsOneSkuPromotion("B", 2, 45.0);
+        List<Sku> skus = new ArrayList<>();
+        skus.add(new Sku("B", 30));
+        skus.add(new Sku("B", 30));
+        double discount = promotion.applyPromotion(skus);
+        assertNotNull(discount);
+        assertEquals(15.0,discount);
     }
 }
